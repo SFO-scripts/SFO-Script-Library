673344b25d027101c661e62d41b56bf0c7aecef0
DrunkFlamingo <matte901@gmail.com>
2018-07-16 19:10:00 -0400

    updates to diplomacy settings from venris

--- script/export_helpers__sfo_starting_diplomacy.lua
+++ script/export_helpers__sfo_starting_diplomacy.lua
@@ -2,9 +2,8 @@ events = get_events(); cm = get_cm(); sfo = _G.sfo
 
 local function sfo_starting_diplomacy()
     sfo:log("Setting starting diplomacy", "sfo_starting_diplomacy")
-    cm:force_make_peace("wh2_main_hef_eataine", "wh2_main_hef_yvresse");
-	cm:force_make_peace("wh2_main_hef_eataine", "wh2_main_hef_cothique");
-	
+
+	cm:force_make_peace("wh_dlc08_nor_norsca", "wh_main_brt_bretonnia");
 	cm:force_make_peace("wh_main_emp_marienburg", "wh_main_brt_bretonnia");	
 	cm:force_make_peace("wh_main_brt_carcassonne", "wh2_main_hef_yvresse");
 	cm:force_make_peace("wh_main_brt_lyonesse", "wh2_main_hef_cothique");
@@ -27,6 +26,9 @@ local function sfo_starting_diplomacy()
 		cm:force_declare_war("wh2_dlc09_tmb_khemri", "wh2_main_vmp_strygos_empire", false, false);
 		cm:force_declare_war("wh_main_grn_bloody_spearz", "wh_main_grn_red_eye", false, false);
 		cm:force_declare_war("wh_dlc03_bst_beastmen", "wh_main_teb_tilea", false, false);
+		cm:force_declare_war("wh2_main_skv_clan_mors", "wh_main_grn_crooked_moon", false, false);
+		cm:force_declare_war("wh2_main_skv_clan_mors", "wh_main_dwf_karak_izor", false, false);
+		cm:force_declare_war("wh_dlc08_nor_vanaheimlings", "wh_main_brt_bretonnia", false, false);
 	end
 
     sfo:log("starting diplomacy set without error.", "sfo_starting_diplomacy")

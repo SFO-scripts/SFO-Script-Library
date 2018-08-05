69adb1b84a884ce751e55d5f6c6a9435ae0ca814
DrunkFlamingo <matte901@gmail.com>
2018-08-05 18:18:03 -0400

    updated scripts

--- script/export_helpers__sfo_k8p.lua
+++ script/export_helpers__sfo_k8p.lua
@@ -12,10 +12,12 @@ function sfo_eight_peaks()
             if not cm:is_multiplayer() then
                 cm:force_declare_war("wh_main_grn_crooked_moon", "wh_main_dwf_karak_azul", false, false);	
                 cm:force_declare_war("wh_main_dwf_karak_ziflin", "wh_dlc05_wef_wydrioth", false, false);
+                cm:force_declare_war("wh2_main_skv_clan_mors", "wh_main_grn_necksnappers", false, false);
+                cm:force_declare_war("wh_main_grn_crooked_moon", "wh_main_grn_necksnappers", false, false);
+                cm:force_declare_war("wh_main_dwf_karak_izor", "wh_main_grn_necksnappers", false, false);
+                cm:force_declare_war("wh2_main_skv_clan_mors", "wh_main_grn_crooked_moon", false, false);
+                cm:force_declare_war("wh2_main_skv_clan_mors", "wh_main_dwf_karak_izor", false, false);
             end
-            cm:force_make_peace("wh_main_grn_crooked_moon", "wh_main_grn_necksnappers");
-            cm:force_make_peace("wh_main_dwf_karak_izor", "wh_main_grn_necksnappers");
-            cm:force_make_peace("wh2_main_skv_clan_mors", "wh_main_grn_necksnappers");
             cm:force_make_trade_agreement("wh_main_dwf_karak_ziflin", "wh_main_dwf_karak_norn");
             cm:force_make_trade_agreement("wh_main_dwf_karak_ziflin", "wh_main_emp_wissenland");
             cm:force_make_peace("wh_main_dwf_karak_ziflin", "wh_main_brt_bastonne");
@@ -32,15 +34,28 @@ function sfo_eight_peaks()
             if not cm:is_multiplayer() then
                 cm:force_declare_war("wh2_main_skv_clan_mors", "wh_main_dwf_karak_azul", false, false);	
                 cm:force_declare_war("wh_main_dwf_karak_ziflin", "wh_dlc05_wef_wydrioth", false, false);
+                cm:force_declare_war("wh2_main_skv_clan_mors", "wh_main_grn_necksnappers", false, false);
+                cm:force_declare_war("wh_main_grn_crooked_moon", "wh_main_grn_necksnappers", false, false);
+                cm:force_declare_war("wh_main_dwf_karak_izor", "wh_main_grn_necksnappers", false, false);
+                cm:force_declare_war("wh2_main_skv_clan_mors", "wh_main_grn_crooked_moon", false, false);
+                cm:force_declare_war("wh2_main_skv_clan_mors", "wh_main_dwf_karak_izor", false, false);
             end
-            cm:force_make_peace("wh_main_grn_crooked_moon", "wh_main_grn_necksnappers");
-            cm:force_make_peace("wh_main_dwf_karak_izor", "wh_main_grn_necksnappers");
-            cm:force_make_peace("wh2_main_skv_clan_mors", "wh_main_grn_necksnappers");
             cm:force_make_peace("wh2_main_skv_clan_mors", "wh2_main_grn_arachnos");
             cm:force_make_trade_agreement("wh_main_dwf_karak_ziflin", "wh_main_dwf_karak_norn");
             cm:force_make_trade_agreement("wh_main_dwf_karak_ziflin", "wh_main_emp_wissenland");		
             cm:force_make_peace("wh_main_dwf_karak_ziflin", "wh_main_brt_bastonne");
             out("finished moving eight peaks characters!")
+            --prevent peace
+            cm:force_diplomacy("faction:wh2_main_skv_clan_mors", "faction:wh_main_dwf_karak_izor", "peace", false, false, false)
+            cm:force_diplomacy("faction:wh2_main_skv_clan_mors", "faction:wh_main_grn_crooked_moon", "peace", false, false, false)
+            cm:force_diplomacy("faction:wh_main_dwf_karak_izor", "faction:wh2_main_skv_clan_mors", "peace", false, false, false)
+            cm:force_diplomacy("faction:wh_main_grn_crooked_moon", "faction:wh2_main_skv_clan_mors", "peace", false, false, false)
+            --allow war with the 8 peaks gits
+            cm:force_diplomacy("faction:wh2_main_skv_clan_mors", "faction:wh_main_grn_necksnappers", "war", true, true, false)
+            cm:force_diplomacy("faction:wh_main_dwf_karak_izor", "faction:wh_main_grn_necksnappers", "war", true, true, false)
+            cm:force_diplomacy("faction:wh_main_grn_crooked_moon", "faction:wh_main_grn_necksnappers", "war", true, true, false)
+
+
         end
     end
 end

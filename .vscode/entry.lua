d049953b61a4681446249244d97dba4ad4205720
DF <matte901@gmail.com>
2018-10-15 16:33:01 -0400

    completed greenskin spawning

--- .vscode/entry.lua
+++ .vscode/entry.lua
@@ -5,7 +5,7 @@ package.seeall = {} --:WHATEVER
 --drunk flamingo library
 require("mod/sfo_lib")
 require("mod/sfo_greentide")
-require("script/export_helpers__sfo_greenskin_mechanic")
+
 --export helpers
 require("script/export_helpers__sfo_k8p")
 require("script/export_helpers__sfo_green_knight")
@@ -13,5 +13,7 @@ require("script/export_helpers__sfo_everchosen_building")
 require("script/export_helpers__sfo_lord_of_change")
 require("script/export_helpers__sfo_starting_diplomacy")
 --require("script/export_helpers__sfo_groups")
+require("script/export_helpers__sfo_greenskin_mechanic")
+require("script/export_helpers__sfo_greenskin_manual_waaghs")
 
 
---
+++ script/export_helpers__sfo_greenskin_manual_waaghs.lua
@@ -0,0 +1,55 @@
+local sfo = _G.sfo
+local events = get_events()
+---[[mr. venris edit this 
+local grn_units = {
+    "wh_main_grn_mon_trolls", 
+    "wh_main_grn_mon_trolls"
+}--:vector<string>
+SFO_CONST_WAAGH_SIZE_BASE = 13 --:number
+SFO_CONST_WAAGH_SIZE_VARIANCE = 6 --:number
+SFO_CONST_WAAGH_ARMY_BUNDLE = "grn_greentide_manual_waagh" --:string
+SFO_CONST_WAAGH_BUNDLE_TIMER = 0 --:number --set to 0 for infinite
+--]]
+
+
+--v function(faction: CA_FACTION)
+local function sfo_spawn_waaagh(faction)
+    local home_region = faction:home_region()
+    if home_region:is_null_interface() or home_region:settlement():is_null_interface() then
+        sfo:log("Failed to find a home region for spawning!")
+        return
+    end
+
+    local unit_list = grn_units[cm:random_number(#grn_units)]
+    for i = 1, SFO_CONST_WAAGH_SIZE_BASE + cm:random_number(SFO_CONST_WAAGH_SIZE_VARIANCE) - 1 do
+        unit_list = unit_list .. ",".. grn_units[cm:random_number(#grn_units)]
+    end
+
+    cm:create_force(
+        faction:name(),
+        unit_list,
+        home_region:name(),
+        home_region:settlement():logical_position_x() - 1,
+        home_region:settlement():logical_position_y() + 1,
+        true,
+        true,
+        function(cqi)
+            cm:apply_effect_bundle_to_characters_force(SFO_CONST_WAAGH_ARMY_BUNDLE, cqi, 0, true)
+        end
+    )
+
+end
+
+core:add_listener(
+    "WaaghDilemma",
+    "DilemmaChoiceMadeEvent",
+    function(context)
+        return context:dilemma() == "grn_greentide_choice_charactercompletedbattle"
+    end,
+    function(context)
+        if context:choice() == 0 then
+            sfo_spawn_waaagh(context:faction())
+        end
+    end,
+    true
+)

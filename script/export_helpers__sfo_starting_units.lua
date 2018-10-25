88d92cecd39c5008b89066e77a27d1f0efad0cce
DrunkFlamingo <matte901@gmail.com>
2018-10-25 19:13:37 -0400

    logging

--- script/export_helpers__sfo_starting_units.lua
+++ script/export_helpers__sfo_starting_units.lua
@@ -9,11 +9,13 @@ CONST_SFO_STARTING_UNITS = {
 } --:map<string, vector<string>>
 
 events.FirstTickAfterWorldCreated[#events.FirstTickAfterWorldCreated+1] = function() 
+    _G.sfo:log("Checking to grant units")
     if cm:is_new_game() then
-        for faction_key, units in pairs(sfo_starting_units) do
+        for faction_key, units in pairs(CONST_SFO_STARTING_UNITS) do
             local faction = cm:get_faction(faction_key)
             if faction then
                 for i = 1, #units do
+                    _G.sfo:log("Granting Units: "..faction_key.." given ".. units[i])
                     cm:grant_unit_to_character(cm:char_lookup_str(faction:faction_leader():cqi()), units[i])
                 end
             end
